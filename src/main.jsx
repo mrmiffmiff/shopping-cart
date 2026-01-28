bd6436c831bc344301b9ee3840ce02025e83bc40
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-01-28 13:20:03 -0800

    move routes to own file

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router";
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import routes from './routes'
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 )
