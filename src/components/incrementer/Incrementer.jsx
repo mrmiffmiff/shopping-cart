fcfdf94fc5ca73fd8e9a0b5a0c435fd19bc59dc4
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-01-28 17:24:42 -0800

    some fixes to match my expected functionality

--- src/components/incrementer/Incrementer.jsx
+++ src/components/incrementer/Incrementer.jsx
@@ -1,14 +1,33 @@
 const Incrementer = ({ currentValue, updateFn }) => {
     function handleChange(newVal) {
-        if (newVal >= 1) {
-            updateFn(newVal)
+        if (typeof newVal !== "number") {
+            if (newVal === "") {
+                updateFn(newVal);
+                return;
+            }
+        }
+        const numVal = Number(newVal)
+        if (numVal >= 1) {
+            updateFn(numVal)
+        }
+    }
+
+    function handleKeyDown(event) {
+        if (event.key === "ArrowUp") {
+            event.preventDefault();
+            const numericValue = currentValue === "" ? 0 : Number(currentValue);
+            handleChange(numericValue + 1);
+        } else if (event.key === "ArrowDown") {
+            event.preventDefault();
+            const numericValue = currentValue === "" ? 0 : Number(currentValue);
+            handleChange(numericValue - 1);
         }
     }
 
     return (
         <div>
             <button type="button" aria-label="Decrement Quantity" onClick={() => handleChange(Number(currentValue - 1))}>-</button>
-            <input type="number" min="1" value={currentValue} onChange={(e) => handleChange(Number(e.target.value))} aria-label="Quantity" />
+            <input type="text" inputMode="numeric" pattern="^$|^[1-9]\d*$" value={currentValue} onChange={(e) => handleChange(e.target.value)} onKeyDown={handleKeyDown} aria-label="Quantity" />
             <button type="button" aria-label="Increment Quantity" onClick={() => handleChange(Number(currentValue + 1))}>+</button>
         </div>
     )
