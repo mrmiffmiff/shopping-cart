0eaf904f3b1d2f82bea82d717a5a75df50b90e97
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-02-03 11:51:37 -0800

    product card more fleshed out, somewhat tested

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
-import styles from './ProductCard.modules.css';
+import styles from './ProductCard.module.css';
 import Incrementer from '../incrementer/Incrementer';
 
-const ProductCard = ({ itemId, itemName, itemImgUrl, addToCartFunc }) => {
+const ProductCard = ({ itemId, itemName, itemImgUrl, itemPrice, addToCartFunc }) => {
     const [amount, setAmount] = useState(1);
+
+    const formattedPrice = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(itemPrice);
 
     return (
         <div>
-            <img src={itemImgUrl ? itemImgUrl : '../../assets/images/Placeholder.png'} alt="Item Image" />
+            <img src={itemImgUrl || '../../assets/images/Placeholder.png'} alt="" /> {/*Empty alt because this is presentational. Blind user doesn't need a description, name is sufficient.*/}
             <h3>{itemName}</h3>
+            <h4>{formattedPrice}</h4>
             <button type='button' onClick={() => addToCartFunc(itemId, amount)}>Add to Cart</button>
             <Incrementer
                 currentValue={amount}
---
+++ src/components/productCard/ProductCard.test.jsx
@@ -0,0 +1,81 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ProductCard from "./ProductCard";
+
+describe("Product Card", () => {
+    it("Renders with proper name and price, and an image exists", () => {
+        const mockFn = vi.fn(); // Needs to exist as a parm
+        render(<ProductCard
+            itemId={1}
+            itemName={"Test Name"}
+            itemImgUrl={""}
+            itemPrice={3.45}
+            addToCartFunc={mockFn}
+        />
+        );
+        const img = screen.getByRole("presentation"); // the role for an img with empty but extant alt text
+        expect(img).toBeVisible()
+        const name = screen.getByRole("heading", { level: 3 });
+        expect(name).toHaveTextContent("Test Name");
+        const price = screen.getByRole("heading", { level: 4 });
+        expect(price).toHaveTextContent("$3.45")
+    });
+
+    it("If button not pressed, add function not called", () => {
+        const mockFn = vi.fn();
+        render(<ProductCard
+            itemId={1}
+            itemName={"Test Name"}
+            itemImgUrl={""}
+            itemPrice={3.45}
+            addToCartFunc={mockFn}
+        />
+        );
+
+        expect(mockFn).not.toHaveBeenCalled();
+    });
+
+    it("Button press calls add function with correct parameters", async () => {
+        const mockFn = vi.fn();
+        const user = userEvent.setup();
+
+        render(<ProductCard
+            itemId={1}
+            itemName={"Test Name"}
+            itemImgUrl={""}
+            itemPrice={3.45}
+            addToCartFunc={mockFn}
+        />
+        );
+
+        const addButton = screen.getByRole("button", { name: /Add to Cart/i });
+
+        await user.click(addButton);
+
+        expect(mockFn).toHaveBeenCalledWith(1, 1);
+    });
+
+    it("Button press calls add function with changed parameters", async () => {
+        const mockFn = vi.fn();
+        const user = userEvent.setup();
+
+        render(<ProductCard
+            itemId={2}
+            itemName={"Test Name"}
+            itemImgUrl={""}
+            itemPrice={3.45}
+            addToCartFunc={mockFn}
+        />
+        );
+
+        const addButton = screen.getByRole("button", { name: /Add to Cart/i });
+        const incrementButton = screen.getByRole("button", { name: /Increment Quantity/i });
+
+        await user.click(incrementButton);
+        await user.click(incrementButton);
+        await user.click(addButton);
+
+        expect(mockFn).toHaveBeenCalledWith(2, 3);
+    });
+});
