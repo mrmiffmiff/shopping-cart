bf8ef27e5365bc8256b0e2c60fc4c776bd76d265
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-02-09 16:43:26 -0800

    outlet routing now that navbar is in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar';
+import { Outlet } from 'react-router';
 
 function App() {
 
   return (
-    <Navbar numItems={5} />
+    <>
+      <Navbar numItems={5} />
+      <Outlet />
+    </>
   )
 }
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,13 @@
 import App from "./App";
+import StorePage from "./components/StorePage/StorePage";
 
 const routes = [
     {
         path: "/",
         element: <App />,
+        children: [
+            { path: "shop", element: <StorePage /> },
+        ]
     },
 ];
 
